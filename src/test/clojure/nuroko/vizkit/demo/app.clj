89ad988c5a4ae20a010c0a83b840e2c8765dc2ce
mikera <mike.r.anderson.13@gmail.com>
2013-09-25 18:37:33 +0800

    Basic app

--- src/test/clojure/nuroko/vizkit/demo/app.clj
+++ src/test/clojure/nuroko/vizkit/demo/app.clj
@@ -1,6 +1,22 @@
 (ns nuroko.vizkit.demo.app
   (:use [mikera.cljutils error])
-  (:use [nuroko.vizkit core]))
+  (:use [nuroko.vizkit core])
+  (:import [javax.swing UIManager JFrame SwingUtilities JButton JPanel]))
+
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* true)
 
 (defn launch []
-  (println "Launching...."))
+  (println "Launching....")
+  
+  (SwingUtilities/invokeLater 
+    (fn []
+      (UIManager/setLookAndFeel "org.pushingpixels.substance.api.skin.SubstanceTwilightLookAndFeel")
+      (let [^JFrame frame (JFrame.)
+            ^JPanel p1 (JPanel.)
+            ^JButton b1 (JButton. "Hello World")
+            ]
+        (.add p1 b1)
+        (.add (.getContentPane frame) p1)
+        (.setVisible frame true))))
+  )
--- src/test/java/nuroko/vizkit/demo/DemoApp.java
+++ src/test/java/nuroko/vizkit/demo/DemoApp.java
@@ -5,6 +5,7 @@ import mikera.cljutils.Clojure;
 public class DemoApp {
 	
 	public static void main (String[] args) {
+		
 		Clojure.require("nuroko.vizkit.demo.app");
 		Clojure.eval("(nuroko.vizkit.demo.app/launch)");
 	}
